bf4d4b31c7856cec3980243a3db39354a575e962
customxx <fveramoran@gmail.com>
2017-02-27 17:53:33 -0300

    segundo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pokemon.destroy_all
+
+Pokemon.create(name: "Charmander", description: "fire ")
+Pokemon.create(name: "Squirtle", description: "water")
+Pokemon.create(name: "Pidgey", description: "normal-flying")
+Pokemon.create(name: "Rattata", description: "normal")
+Pokemon.create(name: "Spearow", description: "normal-flying")
+
